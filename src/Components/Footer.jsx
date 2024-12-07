d10b8d609515962123343ab43777d2bd2cd187b2
mdaleardc <ismailaliamot@gmail.com>
2024-12-07 11:42:54 +0600

    small change

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className="w-full bg-gradient-to-tr from-[#123d] to-[#321d] h-[3rem] sm:h-[2.5rem] text-white px-2 flex items-center fixed bottom-0 z-10">
   <footer className="ml-auto mr-[0.5rem] sm:mr-[2rem] ">
-    Copyright © NewsAPI. All rights reserved 2024. Developed by Mohammed Ismail
+    Copyright © NewsAPI. All rights reserved 2024. Developed by<a href='https://ismaildevcode.netlify.app/' className='hover:text-[#0ef] hover:underline text-[orange]'> Mohammed Ismail</a>
   </footer>
 </div>
     )
